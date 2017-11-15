ecfb75326e67ecb82219612ced8c272957f03947
Sarah Bednar <shb66@pitt.edu>
2017-11-15 14:30:40 -0500

    Created threads

--- Pi.java
+++ Pi.java
@@ -1,12 +1,52 @@
+import java.util.concurrent.atomic.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Pi{
+	
+	public static AtomicLong in = new AtomicLong(0);
+	public static int numThreads;
+	public static int numIterations; 
+	
 	public static void main(String[] args) {
+		
 		
 		if(!twoArgs(args) || !isInteger(args))
 		{
 			System.out.println("USAGE: java Pi <numThreads> <numIterations>");
 			System.exit(1);
 		}
+		numThreads = Integer.parseInt(args[0]);
+		numIterations = Integer.parseInt(args[1]); 
+		
+		// total number of iterations
+		// total number of values "inside" the circle
+		// ratio of points inside to total number of iterations
+		// calculate the value of pi from that.
+
+		// Create an array which will hold numThreads 
+	
+		Thread[] ts = new Thread[numThreads];
+		
+		for (int j = 0; j < numThreads; j++) {
+
+			ts[j] = new Thread(() ->
+					   { 
+					   for(int i = 0; i < numIterations/numThreads; i++) {
+						   double x = ThreadLocalRandom.current().nextDouble(1);
+						   double y = ThreadLocalRandom.current().nextDouble(1);
+						   //inside circle 
+					   }
+					   
+					   });
+		   
+		}
+		try {
+			for (Thread t : ts) t.start();
+			
+			for (Thread t : ts) t.join();
+			
+		} catch (InterruptedException iex) { }
+		
 		
 	}
 
